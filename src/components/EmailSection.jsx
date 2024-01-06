eb838b2a86c2648cf9979e5cd0cae3c9cb0598d3
Solomon Emmanuel Thompson <solomonemmanuelthompson.22cs@licet.ac.in>
2024-01-06 21:50:43 +0530

    Resend mail function commented

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -8,33 +8,33 @@ import Image from 'next/image';
 const EmailSection = () => {
     const [emailSubmitted, setEmailSubmitted] =useState(false);
     const handleSubmit =async (e)=> {
-        e.preventDefault();
-        const data={
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
+        // e.preventDefault();
+        // const data={
+        //     email: e.target.email.value,
+        //     subject: e.target.subject.value,
+        //     message: e.target.message.value,
 
-        }
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send"
+        // }
+        // const JSONdata = JSON.stringify(data);
+        // const endpoint = "/api/send"
 
-        const options ={
+        // const options ={
 
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSONdata,
-        }
-        const response= await fetch(endpoint, options);
-        const resData = await response.json();
-        console.log(resData);
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSONdata,
+        // }
+        // const response= await fetch(endpoint, options);
+        // const resData = await response.json();
+        // console.log(resData);
 
-        if (response.status ===200)
-        {
-            console.log('Message sent.');
-            setEmailSubmitted(true)
-        }
+        // if (response.status ===200)
+        // {
+        //     console.log('Message sent.');
+        //     setEmailSubmitted(true)
+        // }
     }
   return (
     <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
