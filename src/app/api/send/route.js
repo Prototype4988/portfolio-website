d636f3273893f04c930dbb9d02cbcef9143d7bb6
Solomon Emmanuel Thompson <solomonemmanuelthompson.22cs@licet.ac.in>
2024-01-06 21:56:08 +0530

    route.js commented

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,31 +1,31 @@
-//import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
+// //import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-    const{body}= await req.json;
-    const {email,subject, message} =body;
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: ['solomonemmanuelthompson.22cs@licet.ac.in', email],
-      subject: subject,
-      react: (
-        <>
-            <h1>{subject}</h1>
-            <p>Thank you for contacting!</p>
-            <p> New message Submitted</p>
-            <p>
-                {message}
-            </p>
-        </>
-      ),
-    });
+// export async function POST(req, res) {
+//     const{body}= await req.json;
+//     const {email,subject, message} =body;
+//   try {
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: ['solomonemmanuelthompson.22cs@licet.ac.in', email],
+//       subject: subject,
+//       react: (
+//         <>
+//             <h1>{subject}</h1>
+//             <p>Thank you for contacting!</p>
+//             <p> New message Submitted</p>
+//             <p>
+//                 {message}
+//             </p>
+//         </>
+//       ),
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
